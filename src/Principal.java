55049c48ee083c61cc4100e74f861046202580d3
Oscar <oscarcr189@outlook.com>
2024-04-21 17:35:09 -0600

    pelicula clase principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,19 @@
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+
+        System.out.println("Mi pelicula es: " + miPelicula.nombre);
+        System.out.println("La fecha de lanzamiento es de: " + miPelicula.fechaDeLanzamiento);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 180;
+
+        System.out.println("Mi otra pelicula es: " + otraPelicula.nombre);
+        System.out.println("La fecha de lanzamiento es de: " + otraPelicula.fechaDeLanzamiento);
     }
 }
