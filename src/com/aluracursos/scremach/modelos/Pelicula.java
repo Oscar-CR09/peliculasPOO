84a8a347c71e9663b2f5944bc3f230ba263ee3ab
Oscar <oscarcr189@outlook.com>
2024-04-28 12:02:41 -0600

    getter

--- src/com/aluracursos/scremach/modelos/Pelicula.java
+++ src/com/aluracursos/scremach/modelos/Pelicula.java
@@ -1,31 +1,63 @@
 package com.aluracursos.scremach.modelos;
 
 public class Pelicula {
-    String nombre;
-    int fechaDeLanzamiento;
-    int duracionEnMinutos;
-    boolean incluidoEnElPlan;
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
 
     private double sumaDeEvaluaciones;
     private int totalDeLasEvaulaciones;
 
-    int getTotalDeLasEvaluaciones(){
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasEvaluaciones(){
         return totalDeLasEvaulaciones;
 
     }
 
-    void muestraFichaTecnica(){
+    public void muestraFichaTecnica(){
         System.out.println("El nombre de la pelicula es: " + nombre);
         System.out.println("La fecha de lanzamiento es de: " + fechaDeLanzamiento);
         System.out.println("Duracion en minutos: " + duracionEnMinutos);
     }
 
-    void evalua(double nota){
+    public void evalua(double nota){
         sumaDeEvaluaciones +=nota;
         totalDeLasEvaulaciones ++;
     }
 
-    double calculaMedia(){
+    public double calculaMedia(){
         return sumaDeEvaluaciones / totalDeLasEvaulaciones;
     }
 
